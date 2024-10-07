1650bec9a8a6d867bcd3a0b95ecfab690104c024
TimotheeKezer <kezt01@uqat.ca>
2024-10-07 12:16:03 -0400

    Création du gameObject temporaire de la station de recharge
    développement de l'interaction avec la station de recharge

---
+++ Assets/Core/Interactables/SCR_RechargeStation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_RechargeStation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Interact(SCR_Recharge battery) {
+
+        Debug.Log("Replenishing battery...");
+
+    }
+}
